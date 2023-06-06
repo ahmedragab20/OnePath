9fa9bddfbe7230666d20d138d7a49c3d2b5adb98
ahmedragab <ahmedragab20901@gmail.com>
2023-06-07 01:19:10 +0300

    🏗️ App Structure (1)

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1
+      class="py-1 px-3 bg-slate-100 rounded-lg shadow-sm duration-300 hover:scale-110 hover:bg-slate-200 cursor-default"
+    >
+      I ❤️ OnePath
+    </h1>
   </div>
 </template>
